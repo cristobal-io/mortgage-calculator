d8bcab03a64ecc890c51d900e5fcf51fe4ce8e68
cristobal-io <me@cristobal.io>
2015-07-22 16:25:10 +0200

    trying to make work test.html without browserify and testling

--- test/amortization.test.js
+++ test/amortization.test.js
@@ -1,25 +1,39 @@
-var isBrowser = !(typeof module !== "undefined" && module.exports);
-if (!isBrowser) {
-  // non-browser code
-  var expect = require("expect.js"),
-    fixtures = require("./fixtures/amortization.json"),
+(function (root) {
+
+  var expect = root.expect || require("expect.js"),
+    getAmortization, fixtures;
+
+  // This test is meant to run in both, the browser and the CLI
+  if (typeof require !== "undefined") {
     getAmortization = require("../lib/amortization.js");
-}
+    fixtures = require("./fixtures/amortization.json");
+  } else {
+    getAmortization = root.getAmortization;
+    fixtures = root.fixtures;
+  }
+  // var isBrowser = !(typeof module !== "undefined" && module.exports);
+  // if (!isBrowser) {
+  //   // non-browser code
+  //   var expect = require("expect.js"),
+  //     fixtures = require("./fixtures/amortization.json"),
+  //     getAmortization = require("../lib/amortization.js");
+  // }
 
-describe("when using the getAmortization function, it:", function () {
+  describe("when using the getAmortization function, it:", function () {
 
-  it("should expose a function", function () {
-    expect(getAmortization).to.be.a("function");
+    it("should expose a function", function () {
+      expect(getAmortization).to.be.a("function");
+    });
+
+    for (var i = 0; i < fixtures.length; i += 1) {
+      it("should pass test nº " +
+        fixtures[i].explanation, checkFixtures(fixtures[i]));
+    }
   });
 
-  for (var i = 0; i < fixtures.length; i += 1) {
-    it("should pass test nº " +
-      fixtures[i].explanation, checkFixtures(fixtures[i]));
+  function checkFixtures(fixture) {
+    return function () {
+      expect(getAmortization.apply(null, fixture.input)).to.eql(fixture.output);
+    };
   }
-});
-
-function checkFixtures(fixture) {
-  return function () {
-    expect(getAmortization.apply(null, fixture.input)).to.eql(fixture.output);
-  };
-}
+}(this));
