51e21e757308c9b15716e9ace74dd7ae82111996
cristobal-io <me@cristobal.io>
2015-10-09 09:43:48 +0200

    total interest enabled

--- test/calculate_mortgage.test.js
+++ test/calculate_mortgage.test.js
@@ -24,9 +24,9 @@ describe("when using the calculateMortgage function, it:", function () {
       "monthlyExpenses": 800
     })).to.eql({
       maxMonthlyPayment: 432,
-      // monthlyIncome: 2000,
       mortgageTotal: 77894.3150262,
-      totalPriceHouse: 97894.3150262
+      totalPriceHouse: 97894.3150262,
+      totalInterest: 1509.0217746
     });
   });
 
@@ -39,9 +39,9 @@ describe("when using the calculateMortgage function, it:", function () {
       "monthlyExpenses": 800
     })).to.eql({
       maxMonthlyPayment: 432,
-      // monthlyIncome: 2000,
       mortgageTotal: 51579.5234551,
-      totalPriceHouse: 71579.5234551
+      totalPriceHouse: 71579.5234551,
+      totalInterest: 21.5748494
     });
   });
 
@@ -54,9 +54,9 @@ describe("when using the calculateMortgage function, it:", function () {
       "monthlyExpenses": -1
     })).to.eql({
       maxMonthlyPayment: 0.36,
-      // monthlyIncome: 1,
       mortgageTotal: 42.9829362,
-      totalPriceHouse: 42.9829362
+      totalPriceHouse: 42.9829362,
+      totalInterest: 0.0179790
     });
   });
 
